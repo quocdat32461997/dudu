4c5ad6644e0509a2ed48fcf774cd99b287640f3e
Dat Ngo <quocdat32461997@yahoo.com>
2025-07-13 11:01:48 -0500

    Added guide for generating prompts

--- data/generate_prompts.py
+++ data/generate_prompts.py
@@ -6,11 +6,22 @@ from functools import partial
 
 from datasets import load_dataset
 
+"""
+Look at this https://huggingface.co/docs/trl/grpo_trainer#example-1-reward-longer-completions for better understanding.
+history: A, B, C, D
+text: A_product_name, B_product_name, C_product_name
+
+prompt_1: The customer purchased A_product_name, B_product_name, C_product_name in the timely order. A_product_name belongs to category Beauty and has following features or categories. 
+label: <explain>The customer will purchase F_product_name that belongs to category or brand. The product has following features</explain>
+
+prompt_2: The customer purchased A_product_name, B_product_name, C_product_name in the timely order. A_product_name belongs to category Beauty and has following features or categories. 
+label: <explain>The customer will purchase F_product_name that belongs to category or brand. The product has following features</explain>
+"""
 # def create_conversation(content):
 #     # TO-DO: add prompt
 #     return {
 #         "messages": [
-#             {"role": "system"},
+#             {"role": "system", "content": content["system_content"]},
 #             {"role": "user", "content": content["user_content"]},
 #             {"role": "assistant", "content": content["assistant_content"]},
 #         ]
@@ -34,7 +45,7 @@ def generate_prompts(
 
     reviews = load_dataset(
         data_path,  # "McAuley-Lab/Amazon-Reviews-2023",
-        review_data_name,  # "5core_timestamp_Automotive",
+        review_data_name,  # "5core_timestamp_Automotive", replace Automative w/ All_Beauty
         trust_remote_code=True,
     )
     metadata = load_dataset(
@@ -59,6 +70,9 @@ def generate_prompts(
         metadata_partition["product_id"]["brand"] = brand
         metadata_partition["categories"]["categories"] = categories
 
+    # TO-DO: add quantization-prompt
+
+    # Parse transaction history
     prompts = []
     for user_id, product_id, history, timestamp in zip(
         reviews["user_id"][:sample_count],
@@ -66,11 +80,13 @@ def generate_prompts(
         reviews["history"][:sample_count],
         reviews["timestamp"][:sample_count],
     ):
-        review_partition[user_id]["product_id"].append(product_id)
-        review_partition[user_id]["history"].append(history)
+        review_partition[user_id]["product_id"].append(product_id)  # next product
+        review_partition[user_id]["history"].append(history)  # past purchase history
         review_partition[user_id]["timestamp"].append(
             datetime.fromtimestamp(int(timestamp) / 1000)
         )
+
+    # TO-DO: add transaction-related prompts here.
 
     # Save to pickle file
     with open(path_to_save_prompt, "wb") as file:
